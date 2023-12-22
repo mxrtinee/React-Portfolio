72fc5653f72738c4e58c19e41f780020f5acf41b
Martin <mxrtinee@user.noreply.github.com>
2023-12-21 18:38:37 -0800

    Updated Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer className="bg-dark text-light py-3">
+      <div className="container text-center">
+      <h3>Copyright © 2023. All rights are reserved</h3>
+      <a href="https://github.com/mxrtinee" target="_blank" rel="noopener noreferrer" className="text-light mx-2">
+          <i className="fab fa-github fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/martineestrada/" target="_blank" rel="noopener noreferrer" className="text-light mx-2">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="https://twitter.com/_martindev" target="_blank" rel="noopener noreferrer" className="text-light mx-2">
+          <i className="fab fa-twitter fa-2x"></i>
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
