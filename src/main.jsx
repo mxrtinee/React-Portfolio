53890701c95bc25b70f81a496728ffb7d8f923a4
Martin <mxrtinee@user.noreply.github.com>
2023-12-28 21:49:35 -0800

    Added Comments to Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import ReactDOM from 'react-dom/client'
+// Importing ReactDOM for rendering React components
+import ReactDOM from 'react-dom/client';
+// Importing createBrowserRouter and RouterProvider from 'react-router-dom' for routing
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// Importing Bootstrap CSS for styling
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Importing the main App component and other page components
 import App from './App.jsx';
 import Error from './pages/Error.jsx';
 import Projects from './pages/Project.jsx';
@@ -10,33 +14,35 @@ import Contact from './pages/Contact.jsx';
 import About from './pages/About.jsx';
 import Resume from './pages/Resume.jsx';
 
+// Creating a BrowserRouter instance with route configurations
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
-    errorElement: <Error />,
+    element: <App />, // Root component for the entire application
+    errorElement: <Error />, // Error component to be displayed on route errors
     children: [
       {
-        index: true,
-        element: <About />,
+        index: true, // Default route for the root path
+        element: <About />, // Component for the About page
       },
       {
         path: '/Projects',
-        element: <Projects />,
+        element: <Projects />, // Component for the Projects page
       },
       {
         path: '/Resume',
-        element: <Resume />,
+        element: <Resume />, // Component for the Resume page
       },
       {
         path: '/Contact',
-        element: <Contact />,
+        element: <Contact />, // Component for the Contact page
       },
     ],
   },
 ]);
 
+// Rendering the application using ReactDOM.createRoot
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Providing the router to the entire application using RouterProvider
   <RouterProvider router={router} />
 );
-
